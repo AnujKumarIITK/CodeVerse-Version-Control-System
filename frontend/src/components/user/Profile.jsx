73695a4e40e51b42bb24dd4a18070c429e8b8933
Anuj Kumar <anujkumarbro002@gmail.com>
2026-05-24 02:37:35 +0530

    Add Changes

--- frontend/src/components/user/Profile.jsx
+++ frontend/src/components/user/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import Navbar from "../Navbar";
 import { BookIcon, RepoIcon } from "@primer/octicons-react";
